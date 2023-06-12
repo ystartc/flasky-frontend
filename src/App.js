cd4a216917a950f3ba445cbf7c1f0572a3f7a2a5
ystartc <113499920+ystartc@users.noreply.github.com>
2023-06-12 14:55:45 -0700

    created Animal component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Animal from './components/Animal';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='App'>
+      <header>The Sapphire Animal Adoption Agency</header>
+      <h1>Animal Listings</h1>
+      <Animal />
+    </section>
   );
 }
 
---
+++ src/components/Animal.js
@@ -0,0 +1,16 @@
+import './Animal.css';
+
+const Animal = () => {
+  const bunny =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJ9cYz9vU0ZFJbBNUXImW455e-CLYNhXlBrQ&usqp=CAU';
+
+  return (
+    <section className='Animal'>
+      <h2>Animal Name: Fluffy</h2>
+      <p>Species: Bunny</p>
+      <img src={bunny}></img>
+    </section>
+  );
+};
+
+export default Animal;
