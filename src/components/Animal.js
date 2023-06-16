e7e2ca965fbcfaa9badd9d37714229d7e060a2d3
ystartc <113499920+ystartc@users.noreply.github.com>
2023-06-16 13:07:44 -0700

    relocated state to app js and arrange dependencies

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -1,16 +1,45 @@
+import PropTypes from 'prop-types';
+// import { useState } from 'react';
 import './Animal.css';
 
-const Animal = () => {
-  const bunny =
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJ9cYz9vU0ZFJbBNUXImW455e-CLYNhXlBrQ&usqp=CAU';
+const Animal = props => {
+  // const [isBookmarked, setBookmarked] = useState(false);
+  // const bookmarkAnimal = () => {
+  //   setBookmarked(!isBookmarked);
+  //   console.log('We can click on this button');
+  // };
 
+  const toggleBookmark = () => {
+    props.updateBookmark({ ...props, bookmark: !props.bookmark });
+  };
+
+  const altText = `Photo of ${props.name}`;
   return (
     <section className='animal'>
-      <h2>Animal Name: Fluffy</h2>
-      <p>Species: Bunny</p>
-      <img src={bunny}></img>
+      <h2>Animal Name: {props.name}</h2>
+      <p>Species: {props.species}</p>
+      <p>Age: {props.age}</p>
+      {props.photo ? (
+        <img src={props.photo} alt={altText} className='img'></img>
+      ) : (
+        '[No Photo]'
+      )}
+      {/* <img src={props.photo} className='img' alt={altText}></img> */}
+      <button onClick={toggleBookmark}>Bookmark</button>
+      <p>Is bookmarked? {props.bookmark ? 'True' : 'False'}</p>
     </section>
   );
+};
+
+Animal.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string,
+  species: PropTypes.string.isRequired,
+  adopted: PropTypes.bool,
+  age: PropTypes.number,
+  photo: PropTypes.string.isRequired,
+  bookmark: PropTypes.bool,
+  updateBookmark: PropTypes.func,
 };
 
 export default Animal;
--- src/components/AnimalList.js
+++ src/components/AnimalList.js
@@ -1,15 +1,40 @@
 import './AnimalList.css';
-import Animal from './Animal';
+import Animal from './Animal.js';
+import PropTypes from 'prop-types';
 
-const AnimalList = () => {
+const AnimalList = props => {
+  // const listOfAnimals = props.animals;
   return (
-    <section className='animal-list'>
-      <h2>Animal List</h2>
-      <Animal />
-      <Animal />
-      <Animal />
+    <section className='animal-list__list'>
+      {props.animals.map(creature => (
+        <li key={creature.id}>
+          <Animal
+            id={creature.id}
+            name={creature.name}
+            species={creature.species}
+            photo={creature.photo}
+            age={creature.age}
+            bookmark={creature.bookmark}
+            updateBookmark={props.updateBookmark}
+          />
+        </li>
+      ))}
     </section>
   );
+};
+
+AnimalList.propTypes = {
+  animals: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string,
+      species: PropTypes.string.isRequired,
+      adopted: PropTypes.bool,
+      age: PropTypes.number,
+      photo: PropTypes.string.isRequired,
+      updateBookmark: PropTypes.func,
+    })
+  ),
 };
 
 export default AnimalList;
